9291a1baa33d3a796377103327b95e967f1ad15e
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2020-09-28 03:01:51 +0900

    [add]ヘッダー・フッター追加

---
+++ frontend/components/my-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer class="pa-3">
+    <v-spacer />
+    <div>&copy; {{ YearOfIssue }} {{ author }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'MyFooter',
+  data: () => ({
+    YearOfIssue: '2020',
+    author: 'yama_tatsuki',
+  }),
+}
+</script>
---
+++ frontend/components/my-header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- サイドバー -->
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      app
+      disable-resize-watcher
+    >
+      <v-list dense three-line>
+        <v-list-item v-for="item in items" :key="item.to" nuxt :to="item.to">
+          <v-list-item-content>
+            <v-list-item-title>{{ item.label }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- ヘッダー -->
+    <v-app-bar class="header" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-layout justify-center fill-height>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn v-for="item in items" :key="item.to" :to="item.to" text nuxt>
+            {{ item.label }}
+          </v-btn>
+        </v-toolbar-items>
+      </v-layout>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  data: () => ({
+    items: [
+      {
+        to: '/',
+        label: 'Top',
+      },
+      {
+        to: '/users/edit',
+        label: 'メールアドレス変更',
+      },
+      {
+        to: '/users/account',
+        label: 'ログアウト',
+      },
+    ],
+    drawer: false,
+  }),
+}
+</script>
