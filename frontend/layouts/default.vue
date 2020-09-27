2feb1304db348ed6e25bb246df693435c881d3af
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2020-09-28 03:02:15 +0900

    [update]ヘッダー・フッターの表示設定

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,30 @@
 <template>
   <v-app>
+    <!-- ログイン中は非表示 -->
+    <MyHeader v-if="$store.$auth.loggedIn" />
     <v-main>
       <nuxt />
     </v-main>
+    <MyFooter />
   </v-app>
 </template>
+
+<script>
+import MyHeader from '@/components/my-header.vue'
+import MyFooter from '@/components/my-footer.vue'
+
+export default {
+  components: {
+    MyHeader,
+    MyFooter,
+  },
+}
+</script>
+
+<style>
+@media only screen and (min-width: 800px) {
+  .container {
+    max-width: 800px;
+  }
+}
+</style>
